ca27bbe1ec5a4122f5625d2143b6f46dff7b775d
bcusack <brady.cusack@workawayit.com>
2020-08-08 11:43:20 -0400

    Project Started

---
+++ src/main/java/com/idtech/item/ItemFinnicksTrident.java
@@ -0,0 +1,13 @@
+package com.idtech.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemFinnicksTrident extends QuickItem {
+
+    {
+        name = "Finnicks Trident";
+        tab = CreativeTabs.COMBAT;
+        texture = "structuregel";
+    }
+
+}
---
+++ src/main/java/com/idtech/item/ItemWaterTrident.java
@@ -0,0 +1,13 @@
+package com.idtech.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemWaterTrident extends QuickItem {
+
+    {
+        name = "Water Trident";
+        tab = CreativeTabs.COMBAT;
+        texture = "structuregel";
+    }
+
+}
